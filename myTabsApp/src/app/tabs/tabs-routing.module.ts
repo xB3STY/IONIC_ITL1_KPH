ade513694a261b9de9fd65d0117d07b34126c4f0
Ba <basty.asumadu@loremipsum.at>
2026-02-11 11:09:35 +0100

    Add Inline Comments

--- myTabsApp/src/app/tabs/tabs-routing.module.ts
+++ myTabsApp/src/app/tabs/tabs-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
+// Routing configuration for the tabs navigation
+// Each tab has its own child route
+
 const routes: Routes = [
   {
     path: 'tabs',
